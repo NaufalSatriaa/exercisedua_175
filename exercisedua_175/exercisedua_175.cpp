97740ae6eb8e928a33a25fc3d77d2d4d90583069
NaufalSatriaa <satriana0017@gmail.com>
2023-06-05 13:57:52 +0700

    isi method cetakpenerbit pada class pengarang

--- exercisedua_175/exercisedua_175.cpp
+++ exercisedua_175/exercisedua_175.cpp
@@ -60,3 +60,11 @@ void penerbit::cetakPengarang() {
 void pengarang::tambahPenerbit(penerbit* pPenerbit) {
 	daftar_penerbit.push_back(pPenerbit);
 }
+
+void pengarang::cetakPenerbit() {
+	cout << "Daftar pengarang pada penerbit\"" << this->nama << "\":\n";
+	for (auto& a : daftar_penerbit) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
